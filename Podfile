1fa8f9105a031ef508c861eb6ef4ba019946ef93
zhanghui <@ioszhanghui@163.com>
2018-03-13 18:17:00 +0800

    提交

---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Riddle' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+  pod 'YYKit', '~> 1.0.9'
+
+  # Pods for Riddle
+
+  target 'RiddleTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- Riddle/ViewController.m
+++ Riddle/ViewController.m
@@ -7,83 +7,215 @@
 //
 
 #import "ViewController.h"
+#import <AudioToolbox/AudioToolbox.h>
+#import "YYKit.h"
 
 @interface ViewController ()<CAAnimationDelegate>
 
-/*筛子*/
-@property(nonatomic,strong)UILabel * label;
 @property(nonatomic,strong)CALayer * layer;
+/*需要添加的筛子*/
+@property(nonatomic,strong)NSMutableArray * layers;
+/*添加所有的点*/
+@property(nonatomic,strong)NSMutableArray * points;
+
+@property (weak, nonatomic) IBOutlet UILabel *showLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *BGView;
 
 @end
 
+//筛子的个数
+static NSInteger numCount =5;
+
+//距离边缘的距离
+#define  kmargin 8
+
 @implementation ViewController
 
+
+-(NSMutableArray *)layers{
+    if (!_layers) {
+        _layers = [NSMutableArray array];
+    }
+    return _layers;
+}
+
+
+-(NSMutableArray *)points{
+    if (!_points) {
+        _points = [NSMutableArray array];
+    }
+    return _points;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    [self.view addSubview:self.label];
-    
-//    CALayer * layer =[CALayer layer];
-//    layer.backgroundColor = [UIColor redColor].CGColor;
-//    layer.bounds=CGRectMake(100, 100, 100, 100);
-//    layer.anchorPoint=CGPointZero;
-//    layer.position=CGPointMake(0, 0);
-//    [self.view.layer addSublayer:layer];
-//    self.layer= layer;
-    
+ 
 }
 
--(UILabel *)label{
-    if (!_label) {
-        _label=[UILabel new];
-        _label.backgroundColor=[UIColor greenColor];
-        _label.textAlignment=NSTextAlignmentCenter;
-        _label.textColor=[UIColor redColor];
-        _label.frame =CGRectMake(10, 10, 50, 50);
-        _label.text=@"1";
+
+-(void)awakeFromNib{
+    [super awakeFromNib];
+    self.showLabel.font = [UIFont fontWithName:@"Helvetica-Bold" size:30];
+}
+
+- (IBAction)setBtnAction:(id)sender {
+}
+- (IBAction)reduceBtnAction:(id)sender {
+    
+    //最少个数为1
+    if (numCount==1) {
+        return;
     }
-    return _label;
+    numCount--;
+    [self resetLabelContent];
+    NSLog(@"%ld",numCount);
 }
-
-
--(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    [super touchesBegan:touches withEvent:event];
-
-    
-    CABasicAnimation * animation = [CABasicAnimation animationWithKeyPath:@"position"];
-    animation.fromValue=[NSValue valueWithCGPoint:CGPointMake(self.label.center.x, self.label.center.y)];
-    animation.toValue=[NSValue valueWithCGPoint:CGPointMake(200, 300)];
-  
-    CABasicAnimation * ani =[CABasicAnimation animation];
-    ani.keyPath = @"transform.rotation";//动画形式 transform  bounds  KVC的键路径  transform.translation 旋转的键值  transform.scale 缩放 transform.rotation 移动 transform.scale.x -》x方向旋转
-    //结束点
-    ani.toValue = @(M_PI*2);
-    
-    CAAnimationGroup * aniGroup = [CAAnimationGroup animation];
-    aniGroup.animations = @[ani,animation];
-    aniGroup.duration = 2.0f;
-    aniGroup.fillMode = kCAFillModeForwards;
-    aniGroup.removedOnCompletion=NO;
-    aniGroup.repeatCount = 1.f;
-    aniGroup.delegate=self;
-    aniGroup.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
-    //动画结束不移除动画
-    aniGroup.removedOnCompletion =NO;
-    [self.label.layer addAnimation:aniGroup forKey:@"group"];
+#pragma mark 修改文字的内容
+-(void)resetLabelContent{
+    self.showLabel.text = [@(numCount) stringValue];
+}
+- (IBAction)addNumAction:(id)sender {
+    //最多的个数10
+    if (numCount==10) {
+        return;
+    }
+    numCount++;
+    NSLog(@"%ld",numCount);
+    [self resetLabelContent];
+}
+- (IBAction)shakeAction:(id)sender {
+    //播放摇晃的段音频
+    [self playSoundFile:@"shake"];
+    [self removeBeforeLayer];
+    [self removeAllPoints];
+    [self createSaizi];
 }
 
 #pragma mark 动画开始
 -(void)animationDidStart:(CAAnimation *)anim{
+    //播放滚动的音频
+    [self playSoundFile:@"roll"];
+}
+
+#pragma mark 移除之前的layer
+-(void)removeBeforeLayer{
+    for(CALayer * layer in self.layers){
+        [layer removeFromSuperlayer];
+    }
+}
+#pragma mark 移除所有的点
+-(void)removeAllPoints{
+    [self.points removeAllObjects];
+}
+
+#pragma mark 创建筛子
+-(void)createSaizi{
     
-    
+    for (NSInteger i=0; i< numCount; i++) {
+        
+        CALayer * layer =[CALayer layer];
+        layer.backgroundColor = [UIColor clearColor].CGColor;
+        layer.bounds=CGRectMake(0, 0, 60, 60);
+        layer.anchorPoint=CGPointZero;
+        
+        NSString * imageName  = [NSString stringWithFormat:@"SZ_%02d",(arc4random()%5+1)];
+        NSLog(@"imageName***%@",imageName);
+        layer.contents = (__bridge id)[UIImage imageNamed:imageName].CGImage;
+        layer.position=CGPointMake(-60, -60);
+        [self.BGView.layer addSublayer:layer];
+        
+        CABasicAnimation * animation = [CABasicAnimation animationWithKeyPath:@"position"];
+        animation.fromValue=[NSValue valueWithCGPoint:CGPointMake(self.layer.center.x, self.layer.center.y)];
+        NSLog(@"CGPoint***%@",NSStringFromCGPoint([self setSaiziScrollPoint]));
+        animation.toValue=[NSValue valueWithCGPoint:[self setSaiziScrollPoint]];
+        
+        CABasicAnimation * ani =[CABasicAnimation animation];
+        ani.keyPath = @"transform.rotation";//动画形式 transform  bounds  KVC的键路径  transform.translation 旋转的键值  transform.scale 缩放 transform.rotation 移动 transform.scale.x -》x方向旋转
+        //结束点
+        ani.toValue = @(M_PI*2);
+        
+        CAAnimationGroup * aniGroup = [CAAnimationGroup animation];
+        aniGroup.animations = @[ani,animation];
+        aniGroup.duration = 1.0f;
+        aniGroup.fillMode = kCAFillModeForwards;
+        aniGroup.removedOnCompletion=NO;
+        aniGroup.repeatCount = 1.f;
+        aniGroup.delegate=self;
+        aniGroup.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
+        //动画结束不移除动画
+        aniGroup.removedOnCompletion =NO;
+        [layer addAnimation:aniGroup forKey:@"group"];
+        
+        [self.layers addObject:layer];
+    }
 }
 
 #pragma mark 动画结束
 -(void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag{
-    
-    
+  
 }
 
+#pragma mark 播放短频音效
+-(void)playSoundFile:(NSString*)fileName{
+    
+    // 声明要保存音效文件的变量
+    SystemSoundID soundID;
+    
+    // 加载文件
+    NSURL *fileURL = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:fileName ofType:@"mp3"]];
+    AudioServicesCreateSystemSoundID((__bridge CFURLRef)(fileURL), &soundID);
+    
+    // 播放短频音效
+    AudioServicesPlayAlertSound(soundID);
+    
+    // 增加震动效果，如果手机处于静音状态，提醒音将自动触发震动
+    AudioServicesPlayAlertSound(kSystemSoundID_Vibrate);
+}
+
+#pragma mark 获取塞子可滚动的区域
+-(CGPoint)setSaiziScrollPoint{
+    
+    CGFloat width = self.view.width;//宽度
+    CGFloat imageHight = 500;
+    
+    CGFloat imageWidth = 60;//图片宽度
+    //最小x
+    CGFloat minX = imageWidth+ kmargin;
+    //最大X
+    CGFloat maxX = width- imageWidth-kmargin;
+    //最小y
+    CGFloat minY = imageWidth + kmargin;
+    //最小y
+    CGFloat maxY = imageHight-(imageWidth + kmargin);
+    CGPoint point = CGPointMake([self getRandomNumber:minX to:maxX], [self getRandomNumber:minY to:maxY]);
+
+    while([self isRepeatData:point]){
+       point = CGPointMake([self getRandomNumber:minX to:maxX], [self getRandomNumber:minY to:maxY]);
+    }
+    [self.points addObject:NSStringFromCGPoint(point)];
+    
+    return point;
+}
+
+#pragma mark  数据是否重复
+-(BOOL)isRepeatData:(CGPoint)point{
+    
+    BOOL isRepeat = NO;
+    for (NSInteger i=0; i<self.points.count; i++) {
+        
+        CGPoint point1 = CGPointFromString([self.points  objectAtIndex:i]);
+        NSLog(@"%d~~~~~~%d",abs(((int)point.x -(int)point1.x)),(abs((int)point.y - (int)point1.y)));
+        if ((abs(((int)point.x -(int)point1.x)))<70||(abs((int)point.y - (int)point1.y))<70) {
+            isRepeat = YES;
+            break;
+        }
+    }
+    return isRepeat;
+}
+
+#pragma mark获取随机数
+-(float)getRandomNumber:(int)from to:(int)to{
+    return from + arc4random()%(to-from);
+}
 
 
 @end
